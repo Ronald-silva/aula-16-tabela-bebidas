830154908e42fb8dede78b999531405e0f1075f1
RONALD <ronaldsilvajob@yahoo.com>
2023-03-25 11:00:58 -0300

    first commit

---
+++ dados.js
@@ -0,0 +1,73 @@
+let  bebidas  =  [
+    {
+        id : 1,
+        nome : 'agua',
+        descrição : '500ml',
+        quantidade : 12,
+        foto : '',
+    },
+    {
+        id : 2,
+        nome : 'Ypioca',
+        descrição : 'Prata 960ml',
+        quantidade : 24,
+        foto : '',
+    },
+    {
+        id : 3,
+        nome : 'suco',
+        descrição : 'Garrafa 1Lt',
+        quantidade : 6,
+        foto : '',
+    },
+    {
+        id : 4 ,
+        nome : 'Vinho São Braz',
+        descricao : 'garrafão 5L',
+        quantidade : 18,
+        foto : '',
+    },
+    {
+        id : 5 ,
+        nome : 'Energetico',
+        descrição : '1 Lt',
+        quantidade : 20,
+        foto : '',
+    },
+    {
+        id : 6,
+        nome : 'Caldo de cana',
+        descrição : '1 Lt',
+        quantidade : 12,
+        foto : '',
+    },
+    {
+        id : 7 ,
+        nome : 'agua de coco',
+        descrição : '1 Lt',
+        quantidade : 12,
+        foto : '',
+    },
+    {
+        id : 8,
+        nome : 'Leite',
+        descrição : '1 Lt',
+        quantidade : 12,
+        foto : '',
+    },
+    {
+        id : 9,
+        nome : 'Cha',
+        descrição : '1 Lt',
+        quantidade : 6,
+        foto : '',
+    },
+    {
+        id : 10,
+        nome : 'Refrigerante',
+        descrição : '1 Lt',
+        quantidade : 24,
+        foto : '',
+    },
+    
+] 
